a38b750072144f38761c394b68354759837a1ad0
Major Hayden <major@redhat.com>
2023-02-09 17:06:03 -0600

    Try sem for parallel

--- get-images.sh
+++ get-images.sh
@@ -8,11 +8,11 @@ aws ec2 describe-regions --filters Name=opt-in-status,Values=opted-in,opt-in-not
 echo "us-east-1" > regions.txt
 echo "us-east-2" >> regions.txt
 
-function download_image() {
-    aws --region=${1} ec2 describe-images --filters Name=is-public,Values=true
-    # aws --region=${1} ec2 describe-images --filters Name=is-public,Values=true | jq -c > ${1}.json
-}
-
 for REGION in $(cat regions.txt); do
-  parallel -j 2 download_image -- $(cat regions.txt)
+  sem -j 4 aws --region=${REGION} ec2 describe-images \
+    --filters Name=is-public,Values=true | jq -c > ${REGION}.json
 done
+
+sem --wait
+
+zstd -T0 -19 --auto-threads=logical -rm *.json
