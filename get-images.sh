69118d290e786c65784c4fd8bc314c0c4dc4ce45
Major Hayden <major@redhat.com>
2023-02-09 17:30:18 -0600

    That was a little too verbose

--- get-images.sh
+++ get-images.sh
@@ -5,7 +5,7 @@ aws ec2 describe-regions --filters Name=opt-in-status,Values=opted-in,opt-in-not
     | jq -r '.Regions[].RegionName' | sort > regions.txt
 
 for REGION in $(cat regions.txt); do
-  sem -j 5 "aws --region=${REGION} ec2 describe-images --filters Name=is-public,Values=true | jq -c | zstd -vv -T0 -19 --auto-threads=logical - > ${REGION}.json.zst"
+  sem -j 5 "aws --region=${REGION} ec2 describe-images --filters Name=is-public,Values=true | jq -c | zstd -v -T0 -19 --auto-threads=logical - > ${REGION}.json.zst"
 done
 
 sem --wait
