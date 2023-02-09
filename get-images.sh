71d89f39d2c83cd853afeb0c3887ae24d2abcc97
Major Hayden <major@redhat.com>
2023-02-09 16:51:11 -0600

    See if parallel can speed things up

--- get-images.sh
+++ get-images.sh
@@ -4,8 +4,14 @@ set -euxo pipefail
 aws ec2 describe-regions --filters Name=opt-in-status,Values=opted-in,opt-in-not-required \
     | jq -r '.Regions[].RegionName' | sort > regions.txt
 
+# Short circuit with just two regions for now.
+echo "us-east-1" > regions.txt
+echo "us-east-2" >> regions.txt
+
+function download_image() {
+    aws --region=${1} ec2 describe-images --filters Name=is-public,Values=true | jq -c  > ${1}.json
+}
+
 for REGION in $(cat regions.txt); do
-  aws --region=${REGION} ec2 describe-images \
-    --filters Name=is-public,Values=true | jq -c | \
-    zstd -19 -T0 --auto-threads=logical - > ${REGION}.json.zst
+  parallel -j 2 download_image -- $(cat regions.txt)
 done
