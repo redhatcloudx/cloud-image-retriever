fc0e9e5be225e0c5a11f7732ef2489aceee43fd3
Major Hayden <major@redhat.com>
2023-02-09 16:40:46 -0600

    Get all regions

--- get-images.sh
+++ get-images.sh
@@ -4,10 +4,6 @@ set -euxo pipefail
 aws ec2 describe-regions --filters Name=opt-in-status,Values=opted-in,opt-in-not-required \
     | jq -r '.Regions[].RegionName' | sort > regions.txt
 
-# Short circuit with just two regions for now.
-echo "us-east-1" > regions.txt
-echo "us-east-2" >> regions.txt
-
 for REGION in $(cat regions.txt); do
   aws --region=${REGION} ec2 describe-images \
     --filters Name=is-public,Values=true | jq -c | \
