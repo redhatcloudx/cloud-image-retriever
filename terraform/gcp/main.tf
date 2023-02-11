9647ff6dce480e4953c5c84917366804861f56e7
Major Hayden <major@redhat.com>
2023-02-11 14:35:17 -0600

    Add GCP workflow

---
+++ terraform/gcp/main.tf
@@ -0,0 +1,48 @@
+# Source: https://binx.io/2022/09/26/setup-keyless-authentication-to-google-cloud-for-github-actions-using-terraform/
+
+locals {
+  project_id = "major-testing"
+  repo       = "redhatcloudx/cloud-image-retriever" 
+}
+
+resource "google_iam_workload_identity_pool" "github_pool" {
+  project                   = local.project_id
+  workload_identity_pool_id = "github-pool"
+  display_name              = "GitHub pool"
+  description               = "Identity pool for GitHub deployments"
+}
+
+resource "google_iam_workload_identity_pool_provider" "github" {
+  project                            = local.project_id
+  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-provider"
+  attribute_mapping = {
+    "google.subject"       = "assertion.sub"
+    "attribute.actor"      = "assertion.actor"
+    "attribute.aud"        = "assertion.aud"
+    "attribute.repository" = "assertion.repository"
+  }
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account" "github_actions" {
+  project      = local.project_id
+  account_id   = "github-actions"
+  display_name = "Service Account used for GitHub Actions"
+}
+
+resource "google_service_account_iam_member" "workload_identity_user" {
+  service_account_id = google_service_account.github_actions.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/${local.repo}"
+}
+
+output "workload_identity_provider" {
+  value = "${google_iam_workload_identity_pool.github_pool.name}/providers/${google_iam_workload_identity_pool_provider.github.workload_identity_pool_provider_id}"
+}
+
+output "service_account" {
+  value = google_service_account.github_actions.email
+}
