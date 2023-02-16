bbb9d05aaa4707e3b84ce56357f6ef12ce9257da
Major Hayden <major@redhat.com>
2023-02-16 10:52:01 -0600

    Use 5 threads for AWS

--- .github/get-aws-images.sh
+++ .github/get-aws-images.sh
@@ -5,7 +5,7 @@ aws ec2 describe-regions --filters Name=opt-in-status,Values=opted-in,opt-in-not
     | jq -r '.Regions[].RegionName' | sort > regions.txt
 
 for REGION in $(cat regions.txt); do
-  sem -j 10 "aws --region=${REGION} ec2 describe-images --filters Name=is-public,Values=true > ${REGION}-raw.json"
+  sem -j 5 "aws --region=${REGION} ec2 describe-images --filters Name=is-public,Values=true > ${REGION}-raw.json"
 done
 
 sem --wait
